ab9aaf4661b9c2365982b2d948661cb4ab74241a
Peter Wunder <hallo@peterwunder.de>
2016-02-06 09:16:13 +0100

    Added all the box images of the OS X version Added a few icons Added TaskbarItemInfo Also fixed an embarrassing typo

--- Fritz!Status Windows/MainWindow.xaml.cs
+++ Fritz!Status Windows/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,17 +10,34 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace Fritz_Status {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+
+		[DllImport("user32.dll")]
+		private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+		[DllImport("user32.dll")]
+		private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+		private const int GWL_STYLE = -16;
+		private const int WS_MAXIMIZEBOX = 0x10000;
+
+		private void Window_SourceInitialized(object sender, EventArgs e) {
+			// not needed after all
+			//var hwnd = new WindowInteropHelper((Window)sender).Handle;
+			//var value = GetWindowLong(hwnd, GWL_STYLE);
+			//SetWindowLong(hwnd, GWL_STYLE, value & ~WS_MAXIMIZEBOX);
+		}
 
 		public MainWindow() {
 			InitializeComponent();
@@ -38,12 +56,15 @@ namespace Fritz_Status {
 				var overview = await FritzStatus.GetStatus();
 				var boxInfo = await FritzStatus.GetBoxNameAndOS();
 
-				if(overview.line[0].pic.IsNullOrWhiteSpace()) {
+				if(!overview.line[0].pic.IsNullOrWhiteSpace()) {
 					Debug.WriteLine(overview.line[0].pic);
 				} else {
 					Debug.WriteLine(overview.line[0].state);
 				}
-				
+				Debug.WriteLine(overview.line[0].train_state);
+				Debug.WriteLine(overview.line[0].time);
+				Debug.WriteLine(overview.line[0].mode);
+
 				Debug.WriteLine(boxInfo.BoxName);
 				Debug.WriteLine(boxInfo.BoxOS);
 
@@ -51,8 +72,11 @@ namespace Fritz_Status {
 				fbLoadingIn.Stop();
 				fbLoadingOut.Begin();
 
+				this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused;
+
 				isUpdating = false;
 			} else {
+				Debug.WriteLine("Still updating");
 			}			
 		}
 	}
