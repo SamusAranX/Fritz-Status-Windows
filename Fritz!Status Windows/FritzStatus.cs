c456f5eaa2d34f3775c3686f5edb451b545e88ac
Peter Wunder <hallo@peterwunder.de>
2016-02-07 01:39:26 +0100

    This port has now reached feature parity with the OS X version. The only things missing are the proper bullet colors.

--- Fritz!Status Windows/FritzStatus.cs
+++ Fritz!Status Windows/FritzStatus.cs
@@ -1,17 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Script.Serialization;
 
 namespace Fritz_Status {
-	enum FritzConnectionStatus {
+	public enum FritzConnectionStatus {
 		Connected,
 		Training,
 		NotConnected,
@@ -31,30 +29,114 @@ namespace Fritz_Status {
 		public string time { get; set; }
 		public string pic { get; set; } // FRITZ!OS < 6.50 ?
 		public string state { get; set; } // FRITZ!OS >= 6.50 ?
+
+		public FritzConnectionStatus GetConnectionStatus() {
+			if (!pic.IsNullOrWhiteSpace()) {
+				if(pic.Contains("rot")) {
+					return FritzConnectionStatus.NotConnected;
+				} else if (pic.Contains("gelb")) {
+					return FritzConnectionStatus.Training;
+				} else if(pic.Contains("gruen")) {
+					return FritzConnectionStatus.Connected;
+				} else {
+					return FritzConnectionStatus.Unknown;
+				}
+			} else if (!state.IsNullOrWhiteSpace()) {
+				if(state == "ready") {
+					return FritzConnectionStatus.Connected;
+				} else {
+					return FritzConnectionStatus.Unknown;
+				}
+			} else {
+				return FritzConnectionStatus.Unknown;
+			}
+		}
 	}
 
-	public struct BoxNameAndOS {
+	public struct BoxInfo {
 		public string BoxName;
 		public string BoxOS;
+		public string BoxNumber;
+		public bool IsNull; // Too lazy to use Nullables
 	}
 
 
-	static class FritzStatus {
+	public class FritzStatus {
+		const string FRITZ_SID = "http://fritz.box/login_sid.lua";
 		const string FRITZ_HOME = "http://fritz.box/home/home.lua";
 		const string FRITZ_FBOS = "http://fritz.box/home/pp_fbos.lua";
 		const string FRITZ_OVERVIEW = "http://fritz.box/internet/dsl_overview.lua?useajax=1&action=get_data";
 
-		const string SID_REGEX = "\\?sid=(.*)\"";
-		const string BOXNAME_REGEX = "\\[\"PRODUKT_NAME\\\"\\] = \"(.*)\"";
-		const string BOXNAME2_REGEX = "<div>\n<p>(.*)\n";
+		const string SID_REGEX = "<SID>(.*)</SID>";
+		const string CHALLENGE_REGEX = "<Challenge>(.*)</Challenge>";
+
+		const string BOXNAME_REGEX = "<div>\n<p>(.*)\n";
 		const string BOX_OS_REGEX = "</p>\n<p>(.*)\n";
+		const string BOX_NUMBER_REGEX = "(\\d+)";
 
-		static Regex sidRegex = new Regex(SID_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-		static Regex boxNameRegex = new Regex(BOXNAME_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-		static Regex boxName2Regex = new Regex(BOXNAME2_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
-		static Regex boxOSRegex = new Regex(BOX_OS_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+		Regex sidRegex = new Regex(SID_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+		Regex challengeRegex = new Regex(CHALLENGE_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
 
-		public static async Task<DslOverview> GetStatus() {
+		Regex boxNameRegex = new Regex(BOXNAME_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+		Regex boxOSRegex = new Regex(BOX_OS_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+		Regex boxNumberRegex = new Regex(BOX_NUMBER_REGEX, RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+
+		public string SessionID = "";
+
+		public async Task<string> GetSessionID() {
+			HttpWebRequest request = WebRequest.CreateHttp(FRITZ_SID);
+			request.Timeout = 5000;
+
+			try {
+				HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
+
+				using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8)) {
+					string responseText = reader.ReadToEnd();
+
+					var sid = sidRegex.Match(responseText).Groups[1].Value;
+					var challenge = challengeRegex.Match(responseText).Groups[1].Value;
+
+					return sid;
+				}
+			} catch (Exception ex) {
+				Debug.WriteLine(ex);
+				return null;
+			}
+		}
+
+		public async Task<BoxInfo> GetBoxInfo() {
+			var uriBuilder = new UriBuilder(FRITZ_FBOS);
+			var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+			query["sid"] = this.SessionID;
+			uriBuilder.Query = query.ToString();
+			Debug.WriteLine(uriBuilder.ToString());
+
+			HttpWebRequest request = WebRequest.CreateHttp(uriBuilder.ToString());
+			request.Timeout = 10000;
+
+			try {
+				HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
+
+				using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8)) {
+					string responseText = reader.ReadToEnd();
+
+					var boxName = boxNameRegex.Match(responseText).Groups[1].Value;
+					var boxOS = boxOSRegex.Match(responseText).Groups[1].Value;
+					var boxNumber = boxNumberRegex.Match(boxName).Groups[1].Value;
+
+					return new BoxInfo() {
+						BoxName = boxName,
+						BoxOS = boxOS,
+						BoxNumber = boxNumber
+					};
+				}
+			} catch (Exception ex) {
+				Debug.WriteLine(ex);
+				return new BoxInfo() { IsNull = true };
+			}
+		}
+
+		public async Task<DslOverview> GetStatus() {
 			HttpWebRequest request = WebRequest.CreateHttp(FRITZ_OVERVIEW);
 			request.Timeout = 5000;
 
@@ -72,45 +154,6 @@ namespace Fritz_Status {
 			} catch (Exception ex) {
 				Debug.WriteLine(ex);
 				return null;
-			}
-		}
-
-		public static async Task<string> GetBoxName() {
-			HttpWebRequest request = WebRequest.CreateHttp(FRITZ_HOME);
-			request.Timeout = 15000;
-
-			try {
-				HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
-
-				using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8)) {
-					string responseText = reader.ReadToEnd();
-
-					return boxNameRegex.Match(responseText).Groups[1].Value;
-				}
-			} catch (Exception ex) {
-				Debug.WriteLine(ex);
-				return null;
-			}
-		}
-
-		public static async Task<BoxNameAndOS> GetBoxNameAndOS() {
-			HttpWebRequest request = WebRequest.CreateHttp(FRITZ_FBOS);
-			request.Timeout = 10000;
-
-			try {
-				HttpWebResponse response = (HttpWebResponse)(await request.GetResponseAsync());
-
-				using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8)) {
-					string responseText = reader.ReadToEnd();
-
-					return new BoxNameAndOS() {
-						BoxName = boxName2Regex.Match(responseText).Groups[1].Value,
-						BoxOS = boxOSRegex.Match(responseText).Groups[1].Value
-					};
-				}
-			} catch (Exception ex) {
-				Debug.WriteLine(ex);
-				return new BoxNameAndOS();
 			}
 		}
 
--- Fritz!Status Windows/MainWindow.xaml.cs
+++ Fritz!Status Windows/MainWindow.xaml.cs
@@ -1,22 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Shell;
+using System.Windows.Threading;
 
 namespace Fritz_Status {
 	/// <summary>
@@ -24,60 +17,149 @@ namespace Fritz_Status {
 	/// </summary>
 	public partial class MainWindow : Window {
 
-		[DllImport("user32.dll")]
-		private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
-		[DllImport("user32.dll")]
-		private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+		private const string FB_BULLET = "\u26AB\uFE0E";
+		private const string FB_CONN_SPEED = "▼ {0} - {1} ▲";
 
-		private const int GWL_STYLE = -16;
-		private const int WS_MAXIMIZEBOX = 0x10000;
+		private bool isFirstUpdate = true;
+		private FritzStatus fritzStatus;
+		private BoxInfo boxInfo = new BoxInfo { IsNull = true }; // We'll reuse this in all updates after the first
 
-		private void Window_SourceInitialized(object sender, EventArgs e) {
-			// not needed after all
-			//var hwnd = new WindowInteropHelper((Window)sender).Handle;
-			//var value = GetWindowLong(hwnd, GWL_STYLE);
-			//SetWindowLong(hwnd, GWL_STYLE, value & ~WS_MAXIMIZEBOX);
-		}
+		private DispatcherTimer refreshTimer;
+		
+		// Colors need fixing. I don't know what FromScRgb is for, but it sure as heck doesn't work with RGB floats.
+		private Dictionary<FritzConnectionStatus, Color> StatusColors = new Dictionary<FritzConnectionStatus, Color> {
+			{FritzConnectionStatus.NotConnected, Color.FromScRgb(1f, 1f, 0.23f, 0.19f) },
+			{FritzConnectionStatus.Training, Color.FromScRgb(1f, 1f, 0.58f, 0f) },
+			{FritzConnectionStatus.Connected, Color.FromScRgb(1f, 0.3f, 0.85f, 0.39f) },
+			{FritzConnectionStatus.Unknown, Color.FromScRgb(1f, 0.73f, 0.73f, 0.73f) }
+		};
+
+		// ProgressBar animations
+		private Storyboard fbLoadingIn, fbLoadingOut;
 
 		public MainWindow() {
 			InitializeComponent();
 
-			fbBoxName.Text = "\u26AB\uFE0E";
+			fbLoadingIn = (Storyboard)FindResource("fbLoadingIn");
+			fbLoadingOut = (Storyboard)FindResource("fbLoadingOut");
+
+			fritzStatus = new FritzStatus();
 		}
 
-		private bool isUpdating = false;
-		private async void button_Click(object sender, RoutedEventArgs e) {
-			if(!isUpdating) { 
-				isUpdating = true;
-
-				var fbLoadingIn = (Storyboard)TryFindResource("fbLoadingIn");
-				fbLoadingIn.Begin();
-
-				var overview = await FritzStatus.GetStatus();
-				var boxInfo = await FritzStatus.GetBoxNameAndOS();
-
-				if(!overview.line[0].pic.IsNullOrWhiteSpace()) {
-					Debug.WriteLine(overview.line[0].pic);
+		private bool _isUpdating = false;
+		private bool isUpdating {
+			get { return _isUpdating; }
+			set {
+				_isUpdating = value;
+				// This will spam the Output window with a warning, just ignore it
+				if (value) {
+					fbLoadingOut.Stop();
+					fbLoadingIn.Begin();
 				} else {
-					Debug.WriteLine(overview.line[0].state);
+					fbLoadingIn.Stop();
+					fbLoadingOut.Begin();
 				}
-				Debug.WriteLine(overview.line[0].train_state);
-				Debug.WriteLine(overview.line[0].time);
-				Debug.WriteLine(overview.line[0].mode);
+			}
+		}
+		
+		private async void Window_Loaded(object sender, RoutedEventArgs e) {
+			refreshTimer = new DispatcherTimer();
+			refreshTimer.Tick += new EventHandler(refreshTimer_Tick);
+			refreshTimer.Interval = new TimeSpan(0, 0, 15);
 
-				Debug.WriteLine(boxInfo.BoxName);
-				Debug.WriteLine(boxInfo.BoxOS);
+			Debug.WriteLine("Refreshing for the first time");
+			await refreshInfo();
 
-				var fbLoadingOut = (Storyboard)TryFindResource("fbLoadingOut");
-				fbLoadingIn.Stop();
-				fbLoadingOut.Begin();
+			Debug.WriteLine("Starting the DispatcherTimer");
+			refreshTimer.Start();
+		}
 
-				this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused;
+		private async void refreshTimer_Tick(object sender, EventArgs e) {
+			Debug.WriteLine("Tick!");
+			await refreshInfo();
+			Debug.WriteLine("Tock!");
+		}
+
+		private async void button_Click(object sender, RoutedEventArgs e) {
+			await refreshInfo(true);		
+		}
+
+		private async Task refreshInfo(bool manual = false) {
+			var manualModStr = manual ? "manual " : ""; // Add "manual" to "Attempting refresh!" when manually refreshing
+			Debug.WriteLine($"Attempting {manualModStr}refresh!"); // Interpolated strings are fucking awesome, by the way
+
+			if (!isUpdating) {
+				isUpdating = true;
+				
+				DslOverview status = null;
+
+				if (isFirstUpdate) {
+					var sid = await fritzStatus.GetSessionID();
+					if (sid == null) {
+						MessageBox.Show("The request either timed out or simply failed.\nTry restarting the app first, and if that doesn't work, see if the box is faulty.", "Couldn't get session ID", MessageBoxButton.OK, MessageBoxImage.Error);
+						Application.Current.Shutdown();
+					}
+
+					fritzStatus.SessionID = sid;
+
+					boxInfo = await fritzStatus.GetBoxInfo();
+				}
+
+				status = await fritzStatus.GetStatus();
+
+				if(!boxInfo.IsNull && status != null) {
+					Debug.WriteLine("We're done refreshing!");
+
+					switch (status.line[0].GetConnectionStatus()) {
+						case FritzConnectionStatus.Connected:
+							this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+							break;
+						case FritzConnectionStatus.Training:
+							this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused;
+							break;
+						case FritzConnectionStatus.NotConnected:
+							this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.Error;
+							break;
+						case FritzConnectionStatus.Unknown:
+							this.TaskbarItemInfo.ProgressState = TaskbarItemProgressState.None;
+							break;
+					}
+
+					// Set the image and set isFirstUpdate to false
+					if(isFirstUpdate) {
+						try {
+							fbImage.Source = new BitmapImage(new Uri($"pack://application:,,,/{Assembly.GetExecutingAssembly().GetName().Name};component/Box Images/fritzbox{boxInfo.BoxNumber}.png", UriKind.Absolute));
+						} catch {
+							fbImage.Source = new BitmapImage(new Uri($"pack://application:,,,/{Assembly.GetExecutingAssembly().GetName().Name};component/Box Images/generic.png", UriKind.Absolute));
+						}
+
+						isFirstUpdate = false;
+					}
+
+					// Set the box name field
+					Run r = new Run($"{FB_BULLET} ");
+					r.Foreground = new SolidColorBrush(StatusColors[status.line[0].GetConnectionStatus()]);
+					fbBoxName.Inlines.Clear();
+					fbBoxName.Inlines.Add(r);
+					fbBoxName.Inlines.Add(boxInfo.BoxName);
+					
+					// Set the connection info field
+					var trainStateString = status.line[0].train_state;
+					if(status.line[0].GetConnectionStatus() == FritzConnectionStatus.Connected) {
+						trainStateString += $" {status.line[0].time}";
+					}
+					fbConnInfo.Text = trainStateString;
+
+					// Set the connection speed field
+					var speedString = $"DL: {status.ds_rate} – UL: {status.us_rate}";
+					fbConnSpeed.Text = speedString;
+				}
 
 				isUpdating = false;
 			} else {
-				Debug.WriteLine("Still updating");
-			}			
+				Debug.WriteLine("There's still a refresh going on");
+			}
+			
 		}
 	}
 }
